af59f7e02dd4a04cc48197c13a4dde7c4c24563b
solladetzky <solladet@gmail.com>
2018-06-22 21:38:52 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,22 +1,21 @@
 # river.rb
 class River
-attr_writer:length
-attr_writer:countries
-attr_writer:discharge
-  def initialize (name)
+attr_writer :length, :countries, :discharge, :name
+attr_reader :name, :length, :countries, :discharge
+  def initialize(name)
     @name = name
   end
   def flood
-    @discharge * 1.3
+   @discharge = @discharge+@discharge/100*30
   end
   def dry_up
-    @discharge * 0.5
+    @discharge = @discharge/2
   end
 end
 
-river_one = River.new ("The Nile")
-river_two = River.new ("The Mississippi")
-river_three = River.new ("The Amazon")
-river_four = River.new ("The Seine")
-river_five = River.new ("The Yangtze")
-river_siz = River.new ("The Euphrates")
+# river_one = River.new ("The Nile")
+# river_two = River.new ("The Mississippi")
+# river_three = River.new ("The Amazon")
+# river_four = River.new ("The Seine")
+# river_five = River.new ("The Yangtze")
+# river_siz = River.new ("The Euphrates")
