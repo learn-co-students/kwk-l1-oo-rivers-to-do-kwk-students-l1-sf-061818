b29047cf824eb99c5ecf762c6e9a3908c523ae1f
jackielim17 <jxckielim17@gmail.com>
2018-06-22 21:38:54 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,22 @@
 # river.rb
+class River 
+  attr_reader :name, :length, :countries, :discharge, :flood, :dry_up 
+  attr_writer :length, :countries, :discharge, :name, :flood, :dry_up
+  def initialize(name)
+    @name = name 
+  end 
+ def flood
+   @discharge = @discharge+@discharge/100*30
+ end
+ def dry_up 
+   @discharge = @discharge / 2
+ end 
+ end 
+ 
+ river1 = River.new("The Nile")
+ river2 = River.new("The Mississippi")  
+ river3 = River.new("The Amazon")
+ river4 = River.new("The Seine")
+ river5 = River.new("The Yangtze")
+ river6 = River.new("The Euphrates")
+ 
